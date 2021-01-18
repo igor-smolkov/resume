9a2a7efc87a8338147d01e7c62fbd0c11b600616
Igor Smolkov <smolkov.igor@gmail.com>
2021-01-18 21:40:01 +0700

    cards dragging

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -2,12 +2,43 @@ import './card.scss'
 
 export default class Card {
     constructor() {
-        this.elementCard = document.querySelector('.card');
-        this.elementsContactToCopy = this.elementCard.querySelectorAll('a[href="copy/"');
+        this.elementsCard = document.querySelectorAll('.card');
+        this.elementsCardFrontside = document.querySelector('.card_frontside');
+        this.elementsCardBackside = document.querySelector('.card_backside');
+
+        this.draggingElementCard = null;
+
+        this.elementsContactToCopy = this.elementsCardFrontside.querySelectorAll('a[href="copy/"'); //elementsCardFrontside только для одной!
         this.elementsContactToCopy.forEach(elementContactToCopy => {
             elementContactToCopy.addEventListener('click', (e)=>{this.clickToCopy(e)});
         });
+
+        this.elementsCard.forEach(elementCard => {
+            elementCard.addEventListener('pointerdown', (e)=>{this.onPointerDown(e)});
+            document.addEventListener('pointermove', (e)=>{this.onPointerMove(e)});
+            document.addEventListener('pointerup', (e)=>{this.onPointerUp(e)});
+        })
+        // this.elementCard.ondragstart = function() { return false; };
     }
+    onPointerDown(e) {
+        // if (!e.target.classList.contains('card')) return;
+        this.draggingElementCard = e.target.closest('.card'); //сделать уголок для перемещения
+        if (this.draggingElementCard !== null) {
+            this.draggingElementCard.dataset.shiftX = e.clientX - this.draggingElementCard.getBoundingClientRect().left +34;
+            this.draggingElementCard.dataset.shiftY = e.clientY - this.draggingElementCard.getBoundingClientRect().top +30;
+            this.draggingElementCard.classList.add('card_dragged');
+        }
+    }
+    onPointerMove(e) {
+        if (this.draggingElementCard !== null) {
+            this.draggingElementCard.style.left = e.pageX - this.draggingElementCard.dataset.shiftX + 'px';
+            this.draggingElementCard.style.top = e.pageY - this.draggingElementCard.dataset.shiftY + 'px';
+        }
+    }
+    onPointerUp(e) {
+        this.draggingElementCard = null;
+    }
+
     clickToCopy(e) {
         e.preventDefault();
         if (e.target.innerText[0] === '+') {
