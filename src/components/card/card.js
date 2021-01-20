68b96bcd71893068d2c6bb333b6f1c97b9e54db8
Igor Smolkov <smolkov.igor@gmail.com>
2021-01-20 17:16:40 +0700

    drag-field, front&backside flip, cursor

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,42 +1,51 @@
 import './card.scss'
 
 export default class Card {
-    constructor() {
-        this.elementsCard = document.querySelectorAll('.card');
-        this.elementsCardFrontside = document.querySelector('.card_frontside');
-        this.elementsCardBackside = document.querySelector('.card_backside');
+    constructor(element) {
+        this.elementCard = element;
+        this.elementCardFrontside = this.elementCard.querySelector('.card__frontside');
+        this.elementCardBackside = this.elementCard.querySelector('.card__backside');
 
         this.draggingElementCard = null;
 
-        this.elementsContactToCopy = this.elementsCardFrontside.querySelectorAll('a[href="copy/"'); //elementsCardFrontside только для одной!
-        this.elementsContactToCopy.forEach(elementContactToCopy => {
+        this.elementCardFrontside.querySelectorAll('a[href="copy/"').forEach(elementContactToCopy => {
             elementContactToCopy.addEventListener('click', (e)=>{this.clickToCopy(e)});
         });
+        
+        this.elementCard.addEventListener('pointerdown', (e)=>{this.onPointerDown(e)});
+        document.addEventListener('pointermove', (e)=>{this.onPointerMove(e)});
+        document.addEventListener('pointerup', (e)=>{this.onPointerUp(e)});
 
-        this.elementsCard.forEach(elementCard => {
-            elementCard.addEventListener('pointerdown', (e)=>{this.onPointerDown(e)});
-            document.addEventListener('pointermove', (e)=>{this.onPointerMove(e)});
-            document.addEventListener('pointerup', (e)=>{this.onPointerUp(e)});
-        })
-        // this.elementCard.ondragstart = function() { return false; };
+        this.elementCard.addEventListener('dblclick', (e)=>{this.onDblClick(e)})
     }
     onPointerDown(e) {
-        // if (!e.target.classList.contains('card')) return;
-        this.draggingElementCard = e.target.closest('.card'); //сделать уголок для перемещения
+        if (!e.target.classList.contains('drag-field')) return;
+        this.draggingElementCard = e.target.closest('.card');
         if (this.draggingElementCard !== null) {
             this.draggingElementCard.dataset.shiftX = e.clientX - this.draggingElementCard.getBoundingClientRect().left +34;
             this.draggingElementCard.dataset.shiftY = e.clientY - this.draggingElementCard.getBoundingClientRect().top +30;
             this.draggingElementCard.classList.add('card_dragged');
+            this.draggingElementCard.style.zIndex += 2;
+            document.body.classList.add('grabbing');
+            this.draggingElementCard.style.left = e.pageX - this.draggingElementCard.dataset.shiftX-6 + 'px';
+            this.draggingElementCard.style.top = e.pageY - this.draggingElementCard.dataset.shiftY-10 + 'px';
+            window.getSelection().removeAllRanges();
         }
     }
     onPointerMove(e) {
         if (this.draggingElementCard !== null) {
             this.draggingElementCard.style.left = e.pageX - this.draggingElementCard.dataset.shiftX + 'px';
             this.draggingElementCard.style.top = e.pageY - this.draggingElementCard.dataset.shiftY + 'px';
+            window.getSelection().removeAllRanges();
         }
     }
     onPointerUp(e) {
         this.draggingElementCard = null;
+        document.body.classList.remove('grabbing');
+    }
+    onDblClick(e) {
+        this.elementCardFrontside.classList.toggle('card__frontside_none');
+        this.elementCardBackside.classList.toggle('card__backside_none');
     }
 
     clickToCopy(e) {
--- src/script.js
+++ src/script.js
@@ -2,5 +2,10 @@ import '@/style.scss'
 import '@comp/additional-info/additional-info.scss'
 import Card from '@comp/card/card'
 import eduList from '@comp/edu-list/edu-list'
-const card = new Card();
+
+const cards = document.querySelectorAll('.card');
+cards.forEach(cardElem => {
+    const card = new Card(cardElem);
+});
+
 eduList();
